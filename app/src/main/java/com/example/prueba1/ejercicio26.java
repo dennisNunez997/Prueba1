8b6ebca7048a8546829217eab61de4e1acfc68a1
dennis <dennis.nunez@epn.edu.ec>
2019-11-04 17:11:20 -0500

    Dennis Nuñez: ejercicios 26

--- app/src/main/java/com/example/prueba1/ejercicio26.java
+++ app/src/main/java/com/example/prueba1/ejercicio26.java
@@ -2,7 +2,11 @@ package com.example.prueba1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+
+import java.io.IOException;
 
 public class ejercicio26 extends AppCompatActivity {
 
@@ -10,5 +14,17 @@ public class ejercicio26 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ejercicio26);
+
+
+    }
+
+    public void ejecutar(View v) {
+        MediaPlayer mp = new MediaPlayer();
+        try {
+            mp.setDataSource("http://www.javaya.com.ar/recursos/gato.mp3");
+            mp.prepare();
+            mp.start();
+        } catch (IOException e) {
+        }
     }
 }
