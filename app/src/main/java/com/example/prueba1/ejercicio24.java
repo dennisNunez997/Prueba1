55dd8aa14cab47c30792238284bf3a7990877d6a
dennis <dennis.nunez@epn.edu.ec>
2019-11-04 16:50:04 -0500

    Dennis Nuñez: ejercicios 24

--- app/src/main/java/com/example/prueba1/ejercicio24.java
+++ app/src/main/java/com/example/prueba1/ejercicio24.java
@@ -2,13 +2,70 @@ package com.example.prueba1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class ejercicio24 extends AppCompatActivity {
+
+    MediaPlayer mp;
+    Button b5;
+    int posicion = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ejercicio24);
+
+        b5=(Button)findViewById(R.id.button5);
+        Toast.makeText(this,b5.getText().toString(), Toast.LENGTH_LONG).show();
+    }
+
+    public void destruir() {
+        if (mp != null)
+            mp.release();
+    }
+
+    public void iniciar(View v) {
+        destruir();
+        mp = MediaPlayer.create(this, R.raw.fenrir);
+        mp.start();
+        String op = b5.getText().toString();
+        if (op.equals("no reproducir en forma circular"))
+            mp.setLooping(false);
+        else
+            mp.setLooping(true);
+    }
+
+    public void pausar(View v) {
+        if (mp != null && mp.isPlaying()) {
+            posicion = mp.getCurrentPosition();
+            mp.pause();
+        }
+    }
+
+    public void continuar(View v) {
+        if (mp != null && mp.isPlaying() == false) {
+            mp.seekTo(posicion);
+            mp.start();
+        }
+    }
+
+    public void detener(View v) {
+        if (mp != null) {
+            mp.stop();
+            posicion = 0;
+        }
+    }
+
+    public void circular(View v) {
+        detener(null);
+        String op = b5.getText().toString();
+        if (op.equals("no reproducir en forma circular"))
+            b5.setText("reproducir en forma circular");
+        else
+            b5.setText("no reproducir en forma circular");
     }
 }
